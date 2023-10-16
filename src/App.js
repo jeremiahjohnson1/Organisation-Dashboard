0224d98fe535631982beb9e0fe87105051827739
jeremiahjohnson1 <johnsongoodjeremiah1@gmail.com>
2023-10-17 00:50:03 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+ import { ColorModeContext, useMode } from "./theme";
+ import { CssBaseline, ThemeProvider } from "@mui/material";
+ import { Routes, Route } from 'react-router-dom';
+ import Topbar from "./scenes/global/Topbar";
+ import Sidebar from "./scenes/global/Sidebar";
+ import Dashboard from "./scenes/dashboard/index";
+ import Team from "./scenes/team";
+ import CONTACTS from "./scenes/contacts";
+ import Invoices from "./scenes/invoice";
+//  import BAR from "./scenes/bar";
+//  import FORM from "./scenes/line";
+//  import LINE from "./scenes/line";
+//  import PIE from "./scenes/pie"; 
+//  import FAQ from "./scenes/faq";
+//  import GEOGRAPHY from "./scenes/geography";
+//  import CALENDER from "./scenes/calender";
+ 
+ 
+ function App() {
+   const [theme, colorMode] = useMode();
+    return (
+      <ColorModeContext.Provider value={colorMode}>
+         <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <div className="app">
+               <Sidebar/>
+               <main className="content">
+                  <Topbar />
+                  <Routes>
+                      <Route path="/" element={<Dashboard/>}/> 
+                      <Route path="/team/" element={<Team/>}/>
+                      <Route path="/contacts" element={<CONTACTS/>}/> 
+                      <Route path="/invoices" element={<Invoices/>}/> 
+                     {/*
+                     <Route path="/contacts" element={<CONTACTS/>}/> 
+                     <Route path="/bar" element={<BAR/>}/>
+                     <Route path="/form" element={<FORM/>}/>
+                     <Route path="/line" element={<LINE/>}/> 
+                     <Route path="/pie" element={<PIE/>}/> 
+                     <Route path="/faq" element={<FAQ/>}/>
+                     <Route path="/geography" element={<GEOGRAPHY/>}/>
+                     <Route path="/calender" element={<CALENDER/>}/>  */}
+                  </Routes>
+               </main>
+            </div>
+         </ThemeProvider>
+      </ColorModeContext.Provider>
+    );
+ }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+ export default App;
